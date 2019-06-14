ffa2e5fea95fff720fa1966565bb53fcbd2acf79
voracious-blog-1591 <zelayaromie@gmail.com>
2019-06-14 01:21:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ function logWhisper (string) {
 function sayHiToGrandma (string) {
   if (string.toLowerCase()){
     return "I can\'t hear you!"
-  } else if (string.toUpperCase()) {
+  }
+  
+  if (string.toUpperCase()) {
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma") {
+  }
+  
+  if (string === "I love you, Grandma") {
     return "I love you, too."
   }
 }
